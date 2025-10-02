76f2e67e57df3eae190b832049a9f2ee050adb2d
tristanbarrow <tristanmarkbarrow@gmail.com>
2025-10-01 21:06:43 -0600

    add more search params

--- code/scripture-server/src/routes/search.ts
+++ code/scripture-server/src/routes/search.ts
@@ -4,16 +4,34 @@ import { parsePath } from "../utils/parsePath";
 
 export async function search(req: Request, reverse: boolean) {
     const { searchParams } = parsePath(req);
-    const contentIncludes = searchParams.get("text");
-    const pageSize = parseInt(searchParams.get("take") || "20");
     const pageNumber = parseInt(searchParams.get("page") || "0");
+    const pageSize = parseInt(searchParams.get("take") || "20");
+    const contentIncludes = searchParams.get("text");
+    const countWords = searchParams.get("count_words") === "true" || false;
+    const countChars = searchParams.get("count_chars") === "true" || false;
+    const removePunc = searchParams.get("no_punc") === "true" || false;
+    const lower = searchParams.get("lower") === "true" || false;
 
-    return jsonResponse(
-        await getSearchResults({
-            contentIncludes,
-            pageSize,
-            pageNumber,
-            reverse,
-        }),
-    );
+    let verseWordMap = searchParams.get("word_map") === "verse" || false;
+    let searchWordMap = searchParams.get("word_map") === "search" || false;
+
+    if (searchParams.get("word_map") === "both") {
+        verseWordMap = true;
+        searchWordMap = true;
+    }
+
+    let res = await getSearchResults({
+        contentIncludes,
+        removePunc,
+        countWords,
+        countChars,
+        pageNumber,
+        pageSize,
+        verseWordMap,
+        searchWordMap,
+        reverse,
+        lower,
+    });
+
+    return jsonResponse(res);
 }
--- code/scripture-server/src/services/searchService.ts
+++ code/scripture-server/src/services/searchService.ts
@@ -1,35 +1,128 @@
 import { count, search } from "../database/searchRepository";
 import type { VerseRecord } from "../models";
 
+type SearchResultRecord = VerseRecord & {
+    word_count?: number;
+    char_count?: number;
+    word_map?: Record<string, number>;
+};
+
 type SearchResults = {
     count: number;
-    verses: VerseRecord[];
+    verses: SearchResultRecord[];
+    word_map?: Record<string, number>;
 };
 
 type SearchDirtyArgs = {
     contentIncludes?: string | null;
-    pageSize: number;
+    searchWordMap: boolean;
+    verseWordMap: boolean;
+    countWords: boolean;
+    countChars: boolean;
+    removePunc: boolean;
     pageNumber: number;
+    pageSize: number;
     reverse: boolean;
+    lower: boolean;
 };
 
 export const getSearchResults = async ({
     contentIncludes = null,
-    pageSize,
+    searchWordMap,
+    verseWordMap,
+    removePunc,
+    countWords,
+    countChars,
     pageNumber,
+    pageSize,
     reverse,
+    lower,
 }: SearchDirtyArgs): Promise<SearchResults> => {
     if (contentIncludes === null) return { count: 0, verses: [] };
 
-    const result = search({
+    let result = search({
         contentIncludes,
         pageSize: pageSize || 20,
         pageNumber: pageNumber || 0,
         reverse,
-    }) as VerseRecord[];
+    }) as SearchResultRecord[];
+
+    if (countWords) {
+        result = result.map((v) => ({
+            ...v,
+            word_count: v.content.split(" ").length,
+        }));
+    }
+
+    if (countChars) {
+        result = result.map((v) => ({ ...v, char_count: v.content.length }));
+    }
+
+    if (removePunc) {
+        result = result.map((v) => ({
+            ...v,
+            content: removePunctuation(v.content),
+        }));
+    }
+
+    if (lower) {
+        result = result.map((v) => ({
+            ...v,
+            content: v.content.toLowerCase(),
+        }));
+    }
+
+    if (verseWordMap || searchWordMap) {
+        result = result.map((v) => ({
+            ...v,
+            word_map: getWordCountMap(v.content),
+        }));
+    }
+
+    if (searchWordMap) {
+        const word_map = result
+            .map((v) => v.word_map || {})
+            .reduce((combined, current) => {
+                for (const word in current) {
+                    combined[word] =
+                        (combined[word] || 0) + (current[word] || 0);
+                }
+                return combined;
+            });
+        if (!verseWordMap) {
+            result = result.map((v) => ({ ...v, word_map: undefined }));
+        }
+        return {
+            count: count(contentIncludes) as number,
+            verses: result,
+            word_map,
+        };
+    }
 
     return {
         count: count(contentIncludes) as number,
         verses: result,
     };
+};
+
+const getWordCountMap = (text: string) => {
+    const words = removePunctuation(text).toLowerCase().split(" ");
+    const wordCount: Record<string, number> = {};
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (word) {
+            wordCount[word] = (wordCount[word] || 0) + 1;
+        }
+    }
+
+    return wordCount;
+};
+
+const removePunctuation = (sentence: string) => {
+    return sentence
+        .replace(/-/g, " ")
+        .replace(/[^\w\s]|_/g, "")
+        .replace(/\s+/g, " ")
+        .trim();
 };
